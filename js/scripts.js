573fbfeeacbee3937d95a0d904fd507ce0cf943a
Francis Prus and Stephen Newkirk <stephen@>
2016-08-10 16:03:19 -0700

    Clean up indentation and spacing and changed results to display as a list.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,11 @@ $(document).ready(function() {
     var music = $("input:radio[name=style]:checked").val();
     var favoriteColor = $("#color").val();
     var dob = $("#born").val();
-    var result = name + " " + food + " " + music + " " + favoriteColor + " " + dob;
-    $("#output").text(result);
+    $("#nameoutput").append(name);
+    $("#foodoutput").append(food);
+    $("#styleoutput").append(music);
+    $("#coloroutput").append(favoriteColor);
+    $("#doboutput").append(dob);
+    $("#output").show();
   });
 });
